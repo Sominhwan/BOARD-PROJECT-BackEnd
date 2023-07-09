53671d1a55957120a983c0d7281547726d66fa70
Sominhwan <thalsghks@naver.com>
2023-07-10 00:28:20 +0900

    게시판스프링백엔드

---
+++ springBoard/src/main/java/com/example/vue/module/board/controller/BoardController.java
@@ -0,0 +1,37 @@
+package com.example.vue.module.board.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vue.module.board.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/board")
+public class BoardController {
+	private final BoardService boardService;
+	
+	@GetMapping("/test")
+	public List<?> index() {
+		return boardService.boardSearch();
+	}
+	@GetMapping("/test3")
+	public String index3() {
+		return boardService.boardSearch2();
+	}
+//	@PostMapping("/test")
+//	public String index() {
+//		return "zz";
+//	}
+
+	@GetMapping("/test2")
+	public String index2() {
+		return "그래서";
+	}
+}
+
